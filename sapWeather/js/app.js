4dda9e56a7b4cb0d57e23230e461646317518544
gripleaf <425797155@qq.com>
2016-02-24 20:03:25 +0800

    upload new version and structure

---
+++ sapWeather/js/app.js
@@ -0,0 +1,11 @@
+angular.module('frintCA', ["ui.router", "ui.select", "ui.bootstrap",])
+    .config(function ($stateProvider, $urlRouterProvider) {
+        /*$stateProvider
+            .state('login', {
+                url: "/login",
+                templateUrl: 'js/templates/login.tpl.html',
+                controller: 'LoginCtrl'
+            });*/
+      //$urlRouterProvider
+        //.otherwise("/login")
+    });
